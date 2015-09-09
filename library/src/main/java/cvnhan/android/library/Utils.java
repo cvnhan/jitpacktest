81c18ce2561ee0c4c86c21f9c80ddcf0d32b6db7
cvnhan <caovannhan2002@gmail.com>
2015-09-09 22:58:53 +0700

    test more lib

---
+++ library/src/main/java/cvnhan/android/library/Utils.java
@@ -0,0 +1,9 @@
+package cvnhan.android.library;
+
+/**
+ * Created by NhanCao on 09-Sep-15.
+ */
+public class Utils {
+    public static int xor(int a, int b) { return a | b;}
+    public static int mod(int a, int b) { return a%b;}
+}
