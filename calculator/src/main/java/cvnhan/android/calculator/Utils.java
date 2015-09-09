7cbdf3a889f36af510ef04106f6d28030431a519
cvnhan <caovannhan2002@gmail.com>
2015-09-09 22:35:36 +0700

    first commit

---
+++ calculator/src/main/java/cvnhan/android/calculator/Utils.java
@@ -0,0 +1,9 @@
+package cvnhan.android.calculator;
+
+/**
+ * Created by NhanCao on 09-Sep-15.
+ */
+public class Utils {
+    public static int mul(int a, int b) { return a * b;}
+    public static int div(int a, int b) { return a/b;}
+}
